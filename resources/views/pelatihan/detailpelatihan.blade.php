1123914140109285f019a9db3242b331d997e1a4
Ilyas Hidayat Rusdy <79189496+IlyasHidayatR@users.noreply.github.com>
2022-01-21 13:56:36 +0700

    pembaharuan dan paginate

--- resources/views/pelatihan/detailpelatihan.blade.php
+++ resources/views/pelatihan/detailpelatihan.blade.php
@@ -1,6 +1,5 @@
 @extends('dashboard')
 @section('content')
-<<<<<<< HEAD
     <center>
         <form action="/search/pelatihan" method="GET" class="form-inline d-flex justify-content-center md-form form-sm active-cyan-2 mt-4" style="margin-bottom: 60px">
             <div class="input-group mr-3 w-50 border rounded" style="border-color: #0A2E54">
@@ -45,38 +44,4 @@
             </ul>
         </div>
     </div>
-=======
-<div class="mb-4" style="font-family: Comfortaa">
-    <h4><b>{{$pelatihan->TITLE}}</b></h4>
-    <h6 class="text-muted">{{$pelatihan->NAME}} - 04/08, 12:52</h6>
-</div>
-<div class="row" style="margin-bottom: 240px; font-family: Comfortaa">
-    <div class="col-md-8 mr-4 mb-3">
-        <img class="mb-4" src="{{$pelatihan->IMG_PATH}}" alt="" width="90%" height="70%">
-        <h4 class="mt-2 mb-3"><b>Tentang pelatihan</b></h4>
-        <p>{{$pelatihan->DESC}}</p>
-    </div>
-    <div class="col-md">
-        <h6><b>Pelatihan Lainnya</b></h6>
-        <br>
-        <ul class="list-group" style="list-style-type: none">
-        @forelse ($data as $data)
-            <li>
-                <div class="border-0">
-                    <div class="mb-4">
-                        <img class="mr-3 float-left" style="border-radius: 7px" src="{{$data->IMG_PATH}}" alt=""
-                            width="60px" height="50px">
-                        <h6 class="card-title"><b><a style="color: black"
-                                    href="/pelatihan/{{$data->ID_PELATIHAN}}">{{$data->TITLE}}</a></b></h6>
-                        <h6 class="card-subtitle text-muted"><SMall>{{$data->NAME}}</SMall></h6>
-                    </div>
-                </div>
-            </li>
-            @empty
-            <p>data not found</p>
-            @endforelse
-        </ul>
-    </div>
-</div>
->>>>>>> 5e72c66617564d578a5da014120cebddf5d283f6
 @endsection
