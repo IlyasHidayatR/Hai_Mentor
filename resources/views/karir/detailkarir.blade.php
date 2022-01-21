9917ec9a8958b201f157ab1a0c24efbe03fd4535
Ilyas Hidayat Rusdy <79189496+IlyasHidayatR@users.noreply.github.com>
2022-01-21 13:54:49 +0700

    pembaharuan dan paginate

--- resources/views/karir/detailkarir.blade.php
+++ resources/views/karir/detailkarir.blade.php
@@ -1,6 +1,5 @@
 @extends('dashboard')
 @section('content')
-<<<<<<< HEAD
     <center>
         <form action="/search/karir" method="GET" class="form-inline d-flex justify-content-center md-form form-sm active-cyan-2 mt-4" style="margin-bottom: 60px">
             <div class="input-group mr-3 w-50 border rounded" style="border-color: #0A2E54">
@@ -23,27 +22,11 @@
             <h4 class="mt-2 mb-3"><b>Deskripsi Pekerjaan</b></h4>
             <p>{{$karir->DESC}}</p>
         </div>
-=======
-<div class="mb-4" style="font-family: Comfortaa">
-    <h4><b>{{$karir->TITLE}}</b></h4>
-    <h6 class="text-muted">{{$karir->NAME}} - 04/08, 12:52</h6>
-</div>
-<div class="row" style="margin-bottom: 240px; font-family: Comfortaa">
-    <div class="col-md-8 mr-4 mb-3">
-        <img class="mb-4" src="{{$karir->IMG_PATH}}" alt="" width="90%" height="70%">
-        <h4 class="mt-2 mb-3"><b>Deskripsi Pekerjaan</b></h4>
-        <p>{{$karir->DESC}}</p>
-    </div>
->>>>>>> 5e72c66617564d578a5da014120cebddf5d283f6
         <div class="col-md">
             <h6><b>Karir Lainnya</b></h6>
             <br>
             <ul class="list-group" style="list-style-type: none">
-<<<<<<< HEAD
                 @forelse ($data1 as $data)
-=======
-                @forelse ($data as $data)
->>>>>>> 5e72c66617564d578a5da014120cebddf5d283f6
                 <li>
                     <div class="border-0">
                         <div class="mb-4">
@@ -53,15 +36,9 @@
                         </div>
                     </div>
                 </li>
-<<<<<<< HEAD
                 @empty
                 <p>data not found</p>
                 @endforelse
-=======
-            @empty
-            <p>data not found</p>
-            @endforelse
->>>>>>> 5e72c66617564d578a5da014120cebddf5d283f6
             </ul>
         </div>
     </div>
